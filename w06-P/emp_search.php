a8060f6382be1b90e05550e7601c6bce650d0127
yzo0o913 <70620489+yzo0o913@users.noreply.github.com>
2020-10-12 16:41:08 +0900

    Add files via upload

---
+++ w06-P/emp_search.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+     <meta charset="utf-8">
+     <title> 직원 관리 시스템</title>
+ </head>
+
+ <body>
+     <h2><a href="index.php">직원 관리 시스템</a> | 이름으로 직원 사번 찾기</h2>
+     <form action="emp_search_process.php" method="POST">
+         <label>first_name: </label>
+         <input type="text" name="first_name" placeholder="first_name"><br><br>
+
+         <label>last_name: </label>
+         <input type="text" name="last_name" placeholder="last_name"><br><br>
+
+         <input type="submit" value="Search">
+     </form>
+ </body>
+
+ </html>
---
+++ w06-P/emp_search_process.php
@@ -0,0 +1,54 @@
+<?php
+    $link= mysqli_connect("localhost","admin","admin","employees"); 
+
+
+    if(isset($_GET['first_name'])){
+        $filtered_fn = mysqli_real_escape_string($link,$_GET['first_name']);
+    }else{
+        $filtered_fn = mysqli_real_escape_string($link,$_POST['first_name']);
+    }
+    if(isset($_GET['last_name'])){
+        $filtered_ln = mysqli_real_escape_string($link,$_GET['last_name']);
+    }else{
+        $filtered_ln = mysqli_real_escape_string($link,$_POST['last_name']);
+    }
+
+
+    $filtered = array(
+        'first_name' => mysqli_real_escape_string($link,$_POST['first_name']),
+        'last_name' => mysqli_real_escape_string($link,$_POST['last_name'])
+    );
+
+
+    $check_query = "
+        SELECT *
+        FROM employees
+        WHERE first_name='$filtered_fn',last_name='$filtered_ln'
+    ";
+    // print_r($check_query);
+
+
+    $queryt = "
+        SELECT *
+        FROM employees
+    ";
+
+
+    $result = mysqli_query($link,$queryt);
+
+
+    while($row=mysqli_fetch_array($result)){
+        $emp_info=$row['emp_no'];
+    }
+
+
+    // print_r($emp_info);
+
+
+    if($result==false){
+        echo '저장하는 과정에서 문제가 생겼습니다. 관리자에게 문의하세요.';
+        error_log(mysqli_error($link));
+    }else{
+        echo '사번 조회 결과 : ['.$emp_info.']<p>
+        <a href="index.php">돌아가기</a>';
+    }
